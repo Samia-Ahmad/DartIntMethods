b5230974f201504624307825d90dfe37445d69ce
Samia-Ahmad <samiiiaahmad@gmail.com>
2025-04-21 06:16:50 -0700

    update

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  String myname = "samia";
+  int myage= 22;
+  double myhight= 165.5;
+
+
+  print(myname[0].toUpperCase());
+
+  print(myhight.floor());
+  print (myhight.ceil());
+  print(myhight.round());
+  print(myhight.isNegative);
+  print(myhight.abs());
+
+ 
+
+}
